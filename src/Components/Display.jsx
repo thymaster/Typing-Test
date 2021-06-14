59fd2d10df8199e5d7318424cfa45a145730c6ab
Anish Sarkar <anish_pg@ece.nits.ac.in>
2021-06-14 23:08:52 +0530

    wrong word functionality

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,31 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Paragraph } from "./Paragraph";
+import { quotesArray, random, allowedKeys } from "./Quotes";
 import "../App.css";
 
 let interval = null;
+var givenArr = { Paragraph };
+// var strArr = givenArr.split("");
 
 export function Display() {
+  const inputRef = useRef(null);
+  const outputRef = useRef(null);
   const [timerCount, setTimerCount] = useState(10);
   const [started, setStarted] = useState(false);
   const [ended, setEnded] = useState(false);
   const [wordCount, setWordCount] = useState(0);
-  const [characterCount, setCharacterCount] = useState(0);
+  const [index, setIndex] = useState(0);
+  // const [correctIndex, setCorrectIndex] = useState(0);
   const [typingSpeed, setTypingSpeed] = useState(0);
   const [textAreaStatus, setTextAreaStatus] = useState(true);
   const [color, setColor] = useState("#aadaa9");
   const [input, setInput] = useState(""); //for entered text
+  const [isWrong, setIsWrong] = useState(false);
 
   const handleEnd = () => {
     setEnded(true);
     setStarted(false);
-    setTextAreaStatus(false);
+    // setTextAreaStatus(false);
     clearInterval(interval);
   };
 
   const handleStart = () => {
     setStarted(true);
     setEnded(false);
-    setTextAreaStatus(true);
+    // setTextAreaStatus(true);
+    inputRef.current.focus();
     setTimer();
   };
 
@@ -41,11 +49,43 @@ export function Display() {
     }, 1000);
   };
 
+  // const textChangeHandler = (event) => {
+  //   setInput(event.target.value);
+  //   setStarted(true);
+  //   var str = event.target.value.split("");
+  //   setCharacterCount(event.target.value.length);
+  //   // setWordCount(event.target.value.split(" ").filter(Boolean).length);
+  //   let flag = 0;
+  //   for (let i = 0; i < str.length; i++){
+  //     if(str[i] !== strArr[i]){
+  //       flag = 1;
+  //       break;
+  //     }
+  //   }
+
+  //   (flag === 0) ? setColor("#aadaa9") : setColor("#fcbea4");
+  // };
+
   const textChangeHandler = (event) => {
-    setInput(event.target.value);
-    setStarted(true);
-    setCharacterCount(event.target.value.length);
-    setWordCount(event.target.value.split(" ").filter(Boolean).length);
+    event.preventDefault();
+    const { key } = event;
+    const text = { givenArr };
+
+    if (key === text.charAt(index)) {
+      setIndex(index + 1);
+      // setCorrectIndex(correctIndex + 1);
+      setIsWrong(false);
+      outputRef.current.innerHTML += key;
+    }
+    else{
+      if(allowedKeys.includes(key)) {
+        setIsWrong(true);
+        outputRef.current.innerHTML += `<span style{{color: "red"}}>${key}</span>`
+      }
+    }
+
+    const timeRemains = ((60 - timerCount) / 60).toFixed(2);
+    setTypingSpeed(Math.ceil(index / 5 / timeRemains));
   };
 
   // const onKeyDownHandler = (event) => {
@@ -60,10 +100,10 @@ export function Display() {
     };
   }, []);
 
-  const typingSpeedCounter = () => {
-    const timeRemains = ((60 - timerCount) / 60).toFixed(2);
-    setTypingSpeed(Math.ceil(characterCount / 5 / timeRemains));
-  };
+  // const typingSpeedCounter = () => {
+  //   const timeRemains = ((60 - timerCount) / 60).toFixed(2);
+  //   setTypingSpeed(Math.ceil(index / 5 / timeRemains));
+  // };
 
   const timeLimitHandler = () => {
     //when timer comes to 0
@@ -86,7 +126,7 @@ export function Display() {
         </h3>
         <h3 className="count">Total Word Count: {wordCount}</h3>
         <h3>WPM: {typingSpeed}</h3>
-        <h3>Characters Typed: {characterCount}</h3>
+        <h3>Characters Typed: {index}</h3>
       </div>
       <button onClick={handleStart} style={{ cursor: "pointer" }}>
         Start Timer!
@@ -95,17 +135,17 @@ export function Display() {
         Reset!
       </button>
       <div className="paragraph">
-          <Paragraph />
-          <label>Start Typing below...</label>
-          <textarea
-            onChange={textChangeHandler}
-            onKeyUp={typingSpeedCounter}
-            className="typingSpace"
-            rows="7"
-            cols="10"
-            placeholder="type here..."
-          ></textarea>
-        </div>
+        <Paragraph />
+        {/* <label>Start Typing below...</label> */}
+        <textarea
+          onChange={textChangeHandler}
+          // onKeyUp={typingSpeedCounter}
+          className="typingSpace"
+          rows="7"
+          cols="10"
+          placeholder="type here..."
+        ></textarea>
       </div>
+    </div>
   );
 }
--- src/Components/Paragraph.jsx
+++ src/Components/Paragraph.jsx
@@ -1,36 +1,32 @@
 import React from "react";
 
-export function Paragraph() {
-
+export function Paragraph(props) {
   return (
     <div>
-      <div>
-        <p>
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give you a complete
-          account of the system, and expound the actual teachings of the great
-          explorer of the truth, the master-builder of human happiness. No one
-          rejects, dislikes, or avoids pleasure itself, because it is pleasure,
-          but because those who do not know how to pursue pleasure rationally
-          encounter consequences that are extremely painful. Nor again is there
-          anyone who loves or pursues or desires to obtain pain of itself,
-          because it is pain, but because occasionally circumstances occur in
-          which toil and pain can procure him some great pleasure. To take a
-          trivial example, which of us ever undertakes laborious physical
-          exercise, except to obtain some advantage from it? But who has any
-          right to find fault with a man who chooses to enjoy a pleasure that
-          has no annoying consequences, or one who avoids a pain that produces
-          no resultant pleasure? Contrary to popular belief, Lorem Ipsum is not
-          simply random text. It has roots in a piece of classical Latin
-          literature from 45 BC, making it over 2000 years old. Richard
-          McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-          looked up one of the more obscure Latin words, consectetur, from a
-          Lorem Ipsum passage, and going through the cites of the word in
-          classical literature, discovered the undoubtable source. This book is
-          a treatise on the theory of ethics, very popular during the
-          Renaissance.  
-        </p>
-      </div>
+      <p text>
+        But I must explain to you how all this mistaken idea of denouncing
+        pleasure and praising pain was born and I will give you a complete
+        account of the system, and expound the actual teachings of the great
+        explorer of the truth, the master-builder of human happiness. No one
+        rejects, dislikes, or avoids pleasure itself, because it is pleasure,
+        but because those who do not know how to pursue pleasure rationally
+        encounter consequences that are extremely painful. Nor again is there
+        anyone who loves or pursues or desires to obtain pain of itself, because
+        it is pain, but because occasionally circumstances occur in which toil
+        and pain can procure him some great pleasure. To take a trivial example,
+        which of us ever undertakes laborious physical exercise, except to
+        obtain some advantage from it? But who has any right to find fault with
+        a man who chooses to enjoy a pleasure that has no annoying consequences,
+        or one who avoids a pain that produces no resultant pleasure? Contrary
+        to popular belief, Lorem Ipsum is not simply random text. It has roots
+        in a piece of classical Latin literature from 45 BC, making it over 2000
+        years old. Richard McClintock, a Latin professor at Hampden-Sydney
+        College in Virginia, looked up one of the more obscure Latin words,
+        consectetur, from a Lorem Ipsum passage, and going through the cites of
+        the word in classical literature, discovered the undoubtable source.
+        This book is a treatise on the theory of ethics, very popular during the
+        Renaissance.
+      </p>
     </div>
   );
 }
